4bbf24b23331d457dc48e0315e091a2f9656a59a
max <maxunholy@gmail.com>
2021-02-07 17:21:15 +0100

    init commit
    WIP:
    testing of file and folder structures
    testing of installing via composer to check if everything runs as intended

--- src/Pimcore/Bundle/PerspectiveEditorBundle/DependencyInjection/PerspectiveEditorExtension.php
+++ src/Pimcore/Bundle/PerspectiveEditorBundle/DependencyInjection/PerspectiveEditorExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class AdminExtension extends Extension
+class PerspectiveEditorExtension extends Extension
 {
     /**
      * {@inheritdoc}
