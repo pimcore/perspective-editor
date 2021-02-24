71b51a0518f85d24caffc8f62eb641f8b925ca04
Christian Fasching <christian.fasching@pimcore.com>
2021-02-24 12:09:58 +0100

    code cleanup

--- src/Resources/public/js/pimcore/perspective/startup.js
+++ src/Resources/public/js/pimcore/perspective/startup.js
@@ -5,7 +5,6 @@ pimcore.settings.perspectiveview = Class.create({
     panelId: 'perspective_view_panel_id',
     perspectiveTreeStore: null,
     viewTreeStore: null,
-    perspectiveEditPanel: null,
     viewEditPanel: null,
     availablePortlets: [
         {name: t('modificationStatistic'), value: 'pimcore.layout.portlets.modificationStatistic'},
@@ -26,7 +25,6 @@ pimcore.settings.perspectiveview = Class.create({
     },
 
     getTabPanel: function () {
-        console.log(!!this.panel);
         if (!this.panel) {
             this.panel = new Ext.Panel({
                 id: this.panelId,
@@ -70,106 +68,6 @@ pimcore.settings.perspectiveview = Class.create({
         pimcoreTabPanel.setActiveItem(id);
 
         pimcore.layout.refresh();
-    },
-
-    createPerspectiveEditor: function(){
-        this.perspectiveEditPanel = new Ext.Panel({
-            region: "center",
-            width: '75%',
-            autoScroll: true,
-        });
-
-        this.perspectiveTreeStore = new Ext.data.TreeStore({
-            autoLoad: true,
-            proxy: {
-                type: 'ajax',
-                url: '/admin/perspectives-views/perspectives/get-tree',
-                reader: {
-                    type: 'json'
-                }
-            },
-        });
-
-        return new Ext.Panel({
-            title: 'perspective_editor',
-            border: false,
-            layout: 'border',
-            items: [
-                new Ext.tree.Panel({
-                    title: t('plugin_pimcore_perspectiveeditor_available_perspectives'),
-                    region: "west",
-                    autoScroll: true,
-                    animate: false,
-                    containerScroll: true,
-                    width: '25%',
-                    split: true,
-                    store: this.perspectiveTreeStore,
-                    rootVisible: false,
-                    listeners: {
-                        itemclick: function(tree, record){
-                            this.buildPerspectiveEditorPanel(record);
-                        }.bind(this),
-                        itemcontextmenu: function (tree, record, item, index, e, eOpts ) {
-                            e.stopEvent();
-                            var menuItems = this.buildPerspectiveContextMenuItems(record);
-                            if(menuItems.length > 0){
-                                var menu = new Ext.menu.Menu({
-                                    items: menuItems
-                                });
-                                menu.showAt(e.pageX, e.pageY);
-                            }
-                        }.bind(this)
-                    },
-                }),
-                this.perspectiveEditPanel
-            ],
-            bbar: new Ext.toolbar.Toolbar({
-                style: 'background: #e0e1e2',
-                items: [
-                    "->",
-                    new Ext.Button({
-                        text: t("reload"),
-                        iconCls: "pimcore_icon_reload",
-                        handler: function () {
-                            Ext.MessageBox.show({
-                                title: t('are_you_sure'),
-                                msg: t('confirm_reload'),
-                                buttons: Ext.Msg.OKCANCEL,
-                                icon: Ext.MessageBox.INFO,
-                                fn: function (button) {
-                                    if (button === 'ok') {
-                                        this.perspectiveTreeStore.reload();
-                                        this.perspectiveEditPanel.removeAll();
-                                    }
-                                }.bind(this),
-                            });
-                        }.bind(this),
-                    }),
-                    new Ext.Button({
-                        text: t("save"),
-                        iconCls: "pimcore_icon_save",
-                        handler: function () {
-                            Ext.Ajax.request({
-                                url: '/admin/perspectives-views/perspectives/save',
-                                params: {
-                                    data: Ext.JSON.encode(this.perspectiveTreeStore.getRoot().serialize())
-                                },
-                                method: 'POST', success: function (response) {
-                                    responseObject = Ext.decode(response.responseText);
-                                    if (responseObject.success) {
-                                        pimcore.helpers.showNotification(t("success"), t("saved_successfully"), "success");
-                                        this.perspectiveTreeStore.reload();
-                                        this.perspectiveEditPanel.removeAll();
-                                    } else {
-                                        pimcore.helpers.showNotification(t("error"), responseObject.error, "error")
-                                    }
-                                }.bind(this),
-                            });
-                        }.bind(this),
-                    }),
-                ],
-            }),
-        });
     },
 
     pimcoreReady: function(){
