c169b833f431949b4d2623435728c742891e17ed
Christian Fasching <christian.fasching@pimcore.com>
2021-03-08 09:27:25 +0100

    fixed deprecations

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -30,8 +30,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('perspective_editor');
+        $treeBuilder = new TreeBuilder('perspective_editor');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
