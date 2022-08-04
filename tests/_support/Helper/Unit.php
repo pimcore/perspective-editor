818b3b92db6c8d802a16b1a0a9dc8557be59bc8b
robertSt7 <robert.steinkellner@pimcore.com>
2022-08-04 13:46:10 +0200

    Test: correct namespace and remove unused file

--- tests/_support/Helper/Unit.php
+++ tests/_support/Helper/Unit.php
@@ -1,5 +1,5 @@
 <?php
-namespace Pimcore\Bundle\DataImporterBundle\Tests\Helper;
+namespace Pimcore\Bundle\PerspectiveEditorBundle\Tests\Helper;
 
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
