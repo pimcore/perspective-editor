0579a8b1e7b2954c3cdf24f30605f2b5b84d73b3
robertSt7 <robert.steinkellner@pimcore.com>
2022-08-09 09:14:23 +0200

    Tests: Add tests for write configuration

--- tests/Unit/PerspectiveAccessorTest.php
+++ tests/Unit/PerspectiveAccessorTest.php
@@ -3,149 +3,186 @@
 namespace Pimcore\Bundle\PerspectiveEditorBundle\Tests;
 
 
+use Pimcore\Bundle\PerspectiveEditorBundle\Services\AbstractAccessor;
 use Pimcore\Bundle\PerspectiveEditorBundle\Services\PerspectiveAccessor;
 
 class PerspectiveAccessorTest extends \Codeception\Test\Unit
 {
-    public function testConvertTreeStoreToConfiguration()
-    {
-        $accessor = new PerspectiveAccessor("");
-
-        $treeStore = [
-            'parentId' => null,
-            'autoRoot' => true,
-            'id' => 'root',
-            'leaf' => false,
-            'children' => [
-                0 => [
-                    'id' => 'd9ba24f1-ab27-4a4e-9c67-266b2d1a1674',
-                    'name' => 'Catalog',
-                    'type' => 'perspective',
-                    'writeable' => true,
-                    'parentId' => 'root',
-                    'leaf' => false,
-                    'children' => [
-                        0 => [
-                            'id' => 'd9d72c19-747b-4a67-933d-e1c47406f105',
-                            'type' => 'icon',
-                            'leaf' => true,
-                            'writeable' => true,
-                            'config' => [
-                                'iconCls' => null,
-                                'icon' => '/bundles/pimcoreadmin/img/flat-white-icons/book.svg'
-                            ],
-                            'parentId' => 'd9ba24f1-ab27-4a4e-9c67-266b2d1a1674',
+    const TREE_STORE = [
+        'parentId' => null,
+        'autoRoot' => true,
+        'id' => 'root',
+        'leaf' => false,
+        'children' => [
+            0 => [
+                'id' => 'd9ba24f1-ab27-4a4e-9c67-266b2d1a1674',
+                'name' => 'Catalog',
+                'type' => 'perspective',
+                'writeable' => true,
+                'parentId' => 'root',
+                'leaf' => false,
+                'children' => [
+                    0 => [
+                        'id' => 'd9d72c19-747b-4a67-933d-e1c47406f105',
+                        'type' => 'icon',
+                        'leaf' => true,
+                        'writeable' => true,
+                        'config' => [
+                            'iconCls' => null,
+                            'icon' => '/bundles/pimcoreadmin/img/flat-white-icons/book.svg'
                         ],
-                        1 => [
-                            'id' => '7bb849d2-726d-4865-beb4-4f423e6cb3d1',
-                            'type' => 'elementTree',
-                            'leaf' => false,
-                            'writeable' => true,
-                            'parentId' => 'd9ba24f1-ab27-4a4e-9c67-266b2d1a1674',
-                            'children' => [
-                                0 => [
-                                    'id' => '5d88a597-900e-449f-95aa-bc59e8885a06',
-                                    'type' => 'elementTreeElement',
-                                    'leaf' => true,
-                                    'config' => [
-                                        'type' => 'customview',
-                                        'id' => 6,
-                                        'position' => 'left',
-                                        'expanded' => false,
-                                        'hidden' => false,
-                                        'sort' => 0
-                                    ],
-                                    'writeable' => true,
-                                    'parentId' => '7bb849d2-726d-4865-beb4-4f423e6cb3d1',
-                                ]
-                            ]
-                        ],
-                        2 => [
-                            'id' => 'bfe6e35b-d222-4d8e-80ab-6a4fdbbea24a',
-                            'type' => 'elementTreeRight',
-                            'leaf' => false,
-                            'writeable' => true,
-                            'parentId' => 'd9ba24f1-ab27-4a4e-9c67-266b2d1a1674',
-                            'children' => [
-                                0 => [
-                                    'id' => 'faa2974f-4000-4fa2-8686-8e471479c4de',
-                                    'type' => 'elementTreeElement',
-                                    'leaf' => true,
-                                    'config' => [
-                                        'type' => 'asset',
-                                        'position' => 'right',
-                                        'expanded' => false,
-                                        'hidden' => false,
-                                        'sort' => 0
-                                    ],
-                                    'writeable' => true,
-                                    'parentId' => 'bfe6e35b-d222-4d8e-80ab-6a4fdbbea24a',
-                                ],
-                                1 => [
-                                    'id' => '64ba1fd3-7f39-45c8-8631-a8443f124429',
-                                    'type' => 'elementTreeElement',
-                                    'leaf' => true,
-                                    'config' => [
-                                        'type' => 'customview',
-                                        'id' => 1,
-                                        'position' => 'right',
-                                        'expanded' => false,
-                                        'hidden' => false,
-                                        'sort' => 1
-                                    ],
-                                    'writeable' => true,
-                                    'parentId' => 'bfe6e35b-d222-4d8e-80ab-6a4fdbbea24a',
-                                ]
-                            ]
-                        ],
-                        3 => [
-                            'id' => '4e45676d-440f-4c4f-a96f-d81896883ff0',
-                            'type' => 'dashboard',
-                            'leaf' => true,
-                            'config' => [],
-                            'writeable' => true,
-                            'parentId' => 'd9ba24f1-ab27-4a4e-9c67-266b2d1a1674',
-                        ],
-                        4 => [
-                            'id' => 'cfa4be5e-c5de-4dc2-8f96-551a722522f8',
-                            'type' => 'toolbar',
-                            'leaf' => true,
-                            'config' => [
-                                'file' => [
+                        'parentId' => 'd9ba24f1-ab27-4a4e-9c67-266b2d1a1674',
+                    ],
+                    1 => [
+                        'id' => '7bb849d2-726d-4865-beb4-4f423e6cb3d1',
+                        'type' => 'elementTree',
+                        'leaf' => false,
+                        'writeable' => true,
+                        'parentId' => 'd9ba24f1-ab27-4a4e-9c67-266b2d1a1674',
+                        'children' => [
+                            0 => [
+                                'id' => '5d88a597-900e-449f-95aa-bc59e8885a06',
+                                'type' => 'elementTreeElement',
+                                'leaf' => true,
+                                'config' => [
+                                    'type' => 'customview',
+                                    'id' => 6,
+                                    'position' => 'left',
+                                    'expanded' => false,
                                     'hidden' => false,
-                                    'items' => [
-                                        'perspectives' => true,
-                                        'dashboards' => true
-                                    ]
+                                    'sort' => 0
                                 ],
-                                'marketing' => [
-                                    'hidden' => true,
-                                    'items' => [
-                                        'reports' => true,
-                                        'tagmanagemenet' => true,
-                                        'targeting' => true,
-                                        'seo' => [
-                                            'hidden' => false,
-                                            'items' => [
-                                                'documents' => true,
-                                                'robots' => true,
-                                                'httperrors' => true
-                                            ]
+                                'writeable' => true,
+                                'parentId' => '7bb849d2-726d-4865-beb4-4f423e6cb3d1',
+                            ]
+                        ]
+                    ],
+                    2 => [
+                        'id' => 'bfe6e35b-d222-4d8e-80ab-6a4fdbbea24a',
+                        'type' => 'elementTreeRight',
+                        'leaf' => false,
+                        'writeable' => true,
+                        'parentId' => 'd9ba24f1-ab27-4a4e-9c67-266b2d1a1674',
+                        'children' => [
+                            0 => [
+                                'id' => 'faa2974f-4000-4fa2-8686-8e471479c4de',
+                                'type' => 'elementTreeElement',
+                                'leaf' => true,
+                                'config' => [
+                                    'type' => 'asset',
+                                    'position' => 'right',
+                                    'expanded' => false,
+                                    'hidden' => false,
+                                    'sort' => 0
+                                ],
+                                'writeable' => true,
+                                'parentId' => 'bfe6e35b-d222-4d8e-80ab-6a4fdbbea24a',
+                            ],
+                            1 => [
+                                'id' => '64ba1fd3-7f39-45c8-8631-a8443f124429',
+                                'type' => 'elementTreeElement',
+                                'leaf' => true,
+                                'config' => [
+                                    'type' => 'customview',
+                                    'id' => 1,
+                                    'position' => 'right',
+                                    'expanded' => false,
+                                    'hidden' => false,
+                                    'sort' => 1
+                                ],
+                                'writeable' => true,
+                                'parentId' => 'bfe6e35b-d222-4d8e-80ab-6a4fdbbea24a',
+                            ]
+                        ]
+                    ],
+                    3 => [
+                        'id' => '4e45676d-440f-4c4f-a96f-d81896883ff0',
+                        'type' => 'dashboard',
+                        'leaf' => true,
+                        'config' => [],
+                        'writeable' => true,
+                        'parentId' => 'd9ba24f1-ab27-4a4e-9c67-266b2d1a1674',
+                    ],
+                    4 => [
+                        'id' => 'cfa4be5e-c5de-4dc2-8f96-551a722522f8',
+                        'type' => 'toolbar',
+                        'leaf' => true,
+                        'config' => [
+                            'file' => [
+                                'hidden' => false,
+                                'items' => [
+                                    'perspectives' => true,
+                                    'dashboards' => true
+                                ]
+                            ],
+                            'marketing' => [
+                                'hidden' => true,
+                                'items' => [
+                                    'reports' => true,
+                                    'tagmanagemenet' => true,
+                                    'targeting' => true,
+                                    'seo' => [
+                                        'hidden' => false,
+                                        'items' => [
+                                            'documents' => true,
+                                            'robots' => true,
+                                            'httperrors' => true
                                         ]
                                     ]
-                                ],
-                                'ecommerce' => false
+                                ]
                             ],
-                            'writeable' => true,
-                            'parentId' => 'd9ba24f1-ab27-4a4e-9c67-266b2d1a1674',
-                        ]
+                            'ecommerce' => false
+                        ],
+                        'writeable' => true,
+                        'parentId' => 'd9ba24f1-ab27-4a4e-9c67-266b2d1a1674',
                     ]
                 ]
             ]
-        ];
+        ]
+    ];
 
-        $convTreeToConfig =  self::getMethod($accessor, "convertTreeStoreToConfiguration");
-        $config = $convTreeToConfig->invokeArgs($accessor, [$treeStore]);
+    const ERROR_TREE_STORE = [
+        'parentId' => null,
+        'autoRoot' => true,
+        'id' => 'root',
+        'leaf' => false,
+        'children' => [
+            0 => [
+                'id' => 'd9ba24f1-ab27-4a4e-9c67-266b2d1a1674',
+                'name' => 'Catalog',
+                'type' => 'perspective',
+                'writeable' => true,
+                'parentId' => 'root',
+                'leaf' => false,
+                'children' => [
+                    0 => [
+                        'id' => 'd9d72c19-747b-4a67-933d-e1c47406f105',
+                        'type' => 'icon',
+                        'leaf' => true,
+                        'writeable' => true,
+                        'config' => [
+                            'iconCls' => null,
+                            'icon' => '/bundles/pimcoreadmin/img/flat-white-icons/book.svg',
+                            'errorTestField' => '1'
+                        ],
+                        'parentId' => 'd9ba24f1-ab27-4a4e-9c67-266b2d1a1674',
+                    ]
+                ],
+            ]
+        ]
+    ];
+
+    private AbstractAccessor $accessor;
+
+    protected function setUp(): void
+    {
+        $this->accessor = new PerspectiveAccessor("");
+    }
+
+    public function testConvertTreeStoreToConfiguration()
+    {
+        $convTreeToConfig = self::getMethod($this->accessor, "convertTreeStoreToConfiguration");
+        $config = $convTreeToConfig->invokeArgs($this->accessor, [self::TREE_STORE]);
 
         $this->assertEquals(4, count($config['Catalog']));
 
@@ -163,6 +200,17 @@ class PerspectiveAccessorTest extends \Codeception\Test\Unit
         $this->assertEquals(2, count($toolbar['file']['items']));
         $this->assertEquals(3, count($toolbar['marketing']['items']['seo']['items']));
 
+    }
+
+    public function testWriteConfiguration()
+    {
+        $this->accessor->writeConfiguration(self::TREE_STORE, null);
+    }
+
+    public function testWriteConfigurationWithError()
+    {
+        $this->expectException(\Exception::class);
+        $this->accessor->writeConfiguration(self::ERROR_TREE_STORE, null);
     }
 
     private static function getMethod($class, $name)
--- tests/Unit/ViewAccessorTest.php
+++ tests/Unit/ViewAccessorTest.php
@@ -3,62 +3,94 @@
 namespace Pimcore\Bundle\PerspectiveEditorBundle\Tests;
 
 
+use Pimcore\Bundle\PerspectiveEditorBundle\Services\AbstractAccessor;
 use Pimcore\Bundle\PerspectiveEditorBundle\Services\ViewAccessor;
 
 class ViewAccessorTest extends \Codeception\Test\Unit
 {
+    const TREE_STORE = [
+        'parentId' => null,
+        'autoRoot' => true,
+        'id' => 'root',
+        'leaf' => false,
+        'children' => [
+            0 => [
+                'id' => 1,
+                'name' => 'view',
+                'type' => 'view',
+                'leaf' => true,
+                'writeable' => true,
+                'config' => [
+                    'treetype' => 'object',
+                    'name' => 'Cars',
+                    'condition' => null,
+                    'icon' => '/bundles/pimcoreadmin/img/flat-white-icons/automotive.svg',
+                    'id' => 1,
+                    'rootfolder' => '/Product Data/Cars',
+                    'showroot' => false,
+                    'classes' => '0',
+                    'position' => 'left',
+                    'sort' => '3',
+                    'expanded' => true,
+                    'writeable' => true,
+                    'treeContextMenu' => [
+                        'object' => [
+                            'items' => [
+                                'add' => true,
+                                'addFolder' => true,
+                                'importCsv' => true,
+                                'cut' => true,
+                            ]
+                        ],
+                    ]
+                ],
+                'parentId' => 'root'
+            ]
+        ]
+    ];
+
+    const ERROR_TREE_STORE = [
+        'leaf' => false,
+        'children' => [
+            0 => [
+                'id' => 1,
+                'name' => 'view',
+                'type' => 'view',
+                'leaf' => true,
+                'writeable' => true,
+                'config' => [
+                    'treetype' => 'object',
+                    'errorTestField' => 1,
+                ],
+                'parentId' => 'root'
+            ]
+        ]
+    ];
+
+    private AbstractAccessor $accessor;
+
+    protected function setUp(): void {
+        $this->accessor = new ViewAccessor("");
+    }
+
     public function testConvertTreeStoreToConfiguration()
     {
-        $accessor = new ViewAccessor("");
-
-        $treeStore = [
-            'parentId' => null,
-            'autoRoot' => true,
-            'id' => 'root',
-            'leaf' => false,
-            'children' => [
-                0 => [
-                    'id' => 1,
-                    'name' => 'view',
-                    'type' => 'view',
-                    'leaf' => true,
-                    'writeable' => true,
-                    'config' => [
-                        'treetype' => 'object',
-                        'name' => 'Cars',
-                        'condition' => null,
-                        'icon' => '/bundles/pimcoreadmin/img/flat-white-icons/automotive.svg',
-                        'id' => 1,
-                        'rootfolder' => '/Product Data/Cars',
-                        'showroot' => false,
-                        'calsses' => '0',
-                        'position' => 'left',
-                        'sort' => '3',
-                        'expanded' => true,
-                        'writeable' => true,
-                        'treeContextMenu' => [
-                            'object' => [
-                                'items' => [
-                                    'add' => true,
-                                    'addFolder' => true,
-                                    'importCsv' => true,
-                                    'cut' => true,
-                                ]
-                            ],
-                        ]
-                    ],
-                    'parentId' => 'root'
-                ]
-            ]
-        ];
-
-        $convTreeToConfig =  self::getMethod($accessor, "convertTreeStoreToConfiguration");
-        $config = $convTreeToConfig->invokeArgs($accessor, [$treeStore]);
+        $convTreeToConfig =  self::getMethod($this->accessor, "convertTreeStoreToConfiguration");
+        $config = $convTreeToConfig->invokeArgs($this->accessor, [self::TREE_STORE]);
 
         $this->assertEquals(13, count($config[1]));
         $this->assertEquals('object', $config[1]['treetype']);
         $this->assertEquals(4, count($config[1]['treeContextMenu']['object']['items']));
         $this->assertEquals(true, $config[1]['treeContextMenu']['object']['items']['addFolder']);
+    }
+
+    public function testWriteConfiguration() {
+        $this->accessor->writeConfiguration(self::TREE_STORE, null);
+    }
+
+    public function testWriteConfigurationWithError() {
+        $this->expectException(\Exception::class);
+        $this->accessor->writeConfiguration(self::ERROR_TREE_STORE, null);
     }
 
     private static function getMethod($class, $name)
