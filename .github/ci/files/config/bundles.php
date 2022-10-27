9bc7e48d4a76a44616cad49d954951faffd6edf2
Aashan Ghimire <ashanghimire10@gmail.com>
2022-10-27 13:11:34 +0545

    [Actions]: Removed extensions.php in favor of bundles.php (#99)

---
+++ .github/ci/files/config/bundles.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'Pimcore\\Bundle\\EcommerceFrameworkBundle\\PimcoreEcommerceFrameworkBundle' => ['all' => false],
+    'Pimcore\\Bundle\\PerspectiveEditorBundle\\PimcorePerspectiveEditorBundle' => ['all' => true],
+];
