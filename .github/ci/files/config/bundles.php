9e4be1d3b9b767cf0afd3ff8ac8a82882b3987ba
Marco Perberschlager <marco.perberschlager@pimcore.com>
2023-04-25 17:50:59 +0200

    Fixed typo

--- .github/ci/files/config/bundles.php
+++ .github/ci/files/config/bundles.php
@@ -8,5 +8,5 @@ if(\Pimcore\Version::getMajorVersion() >= 11) {
 }
 
 return [
-    \Pimcore\Bundle\PerspectiveEditor\PimcorePerspectiveEditorBundle::class => ['all' => true]
+    \Pimcore\Bundle\PerspectiveEditorBundle\PimcorePerspectiveEditorBundle::class => ['all' => true]
 ];
