c8202b197f9193a0874951e54b4a683fac521315
Marco Perberschlager <marco.perberschlager@pimcore.com>
2023-04-26 09:41:30 +0200

    Added namespace

--- src/DependencyInjection/PimcorePerspectiveEditorExtension.php
+++ src/DependencyInjection/PimcorePerspectiveEditorExtension.php
@@ -16,6 +16,7 @@
 namespace Pimcore\Bundle\PerspectiveEditorBundle\DependencyInjection;
 
 use Pimcore\Bundle\AdminBundle\PimcoreAdminBundle;
+use Pimcore\HttpKernel\Bundle\DependentBundleInterface;
 use Pimcore\HttpKernel\BundleCollection\BundleCollection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
