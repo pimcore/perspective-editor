76771a2c2ab0d98606fc9489a3971228fda8f3b4
JiaJia Ji <kingjia90@gmail.com>
2023-05-11 15:06:23 +0200

    chore: refactor addEventListenerCompatibilityForPlugins

--- src/Resources/public/js/pimcore/perspective/events.js
+++ src/Resources/public/js/pimcore/perspective/events.js
@@ -25,12 +25,11 @@ pimcore.events.onPerspectiveEditorLoadStructureForPermissions = "pimcore.perspec
 
 
 
-//TODO: delete in Pimcore11 and update dependency in composer.json
+//TODO: delete once support for Pimcore 10.6 is dropped
 if(typeof addEventListenerCompatibilityForPlugins === "function") {
     let eventMappings = [];
     eventMappings["onPerspectiveEditorLoadPermissions"] = pimcore.events.onPerspectiveEditorLoadPermissions;
     eventMappings["onPerspectiveEditorLoadStructureForPermissions"] = pimcore.events.onPerspectiveEditorLoadStructureForPermissions;
     addEventListenerCompatibilityForPlugins(eventMappings);
-} else {
-    console.error("Delete addEventListenerCompatibilityForPlugins in the perspective-editor")
+    console.warn("Deprecation: addEventListenerCompatibilityForPlugins will be not supported in Pimcore 11.");
 }
