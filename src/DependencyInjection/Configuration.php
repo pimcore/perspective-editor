29d6f4479ee187b90516687966965c4127d38ff2
Alex Zamponi <562324+alexz707@users.noreply.github.com>
2025-04-09 13:40:59 +0200

    Refactor methods to include return types to remove deprecations

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -28,7 +28,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritdoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('perspective_editor');
 
