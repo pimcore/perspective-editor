f66ff1e6e71e0e8769cc6ceafe1c18c96d6e2842
Christian Fasching <christian.fasching@pimcore.com>
2025-04-30 06:51:31 +0200

    License Switch to POCL

--- src/PimcorePerspectiveEditorBundle.php
+++ src/PimcorePerspectiveEditorBundle.php
@@ -1,16 +1,13 @@
 <?php
 
 /**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - GNU General Public License version 3 (GPLv3)
- * - Pimcore Commercial License (PCL)
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
  * Full copyright and license information is available in
  * LICENSE.md which is distributed with this source code.
  *
- *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
  */
 
 namespace Pimcore\Bundle\PerspectiveEditorBundle;
@@ -32,6 +29,7 @@ class PimcorePerspectiveEditorBundle extends AbstractPimcoreBundle implements Pi
     use PackageVersionTrait;
 
     const PERMISSION_PERSPECTIVE_EDITOR = 'perspective_editor';
+
     const PERMISSION_PERSPECTIVE_EDITOR_VIEW_EDIT = 'perspective_editor_view_edit';
 
     public function getContainerExtension(): ExtensionInterface
