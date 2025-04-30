84ace142bcdc31e4a9bca2ceeb99fd09d851f431
Christian Fasching <fashxp@users.noreply.github.com>
2025-04-30 09:22:05 +0200

    License Switch to POCL (#177)
    * License Switch to POCL
    
    * License Switch to POCL
    
    * fix php-style fixer

--- src/Controller/PerspectiveController.php
+++ src/Controller/PerspectiveController.php
@@ -1,16 +1,13 @@
 <?php
 
 /**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - GNU General Public License version 3 (GPLv3)
- * - Pimcore Commercial License (PCL)
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
  * Full copyright and license information is available in
  * LICENSE.md which is distributed with this source code.
  *
- *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
  */
 
 namespace Pimcore\Bundle\PerspectiveEditorBundle\Controller;
@@ -96,6 +93,7 @@ class PerspectiveController extends UserAwareController
             'success' => true,
             'error' => null
         ];
+
         try {
             $treeStore = json_decode($request->request->getString('data'), true);
             $deletedRecords = json_decode($request->request->getString('deletedRecords'), true);
@@ -124,6 +122,7 @@ class PerspectiveController extends UserAwareController
             'success' => true,
             'error' => null
         ];
+
         try {
             $treeStore = json_decode($request->request->getString('data'), true);
             $deletedRecords = json_decode($request->request->getString('deletedRecords'), true);
