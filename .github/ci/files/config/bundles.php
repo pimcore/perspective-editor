56a0418f8095d4530a46238af52a39c32ac576f5
Bernhard Rusch <brusch@users.noreply.github.com>
2025-05-27 14:53:28 +0200

    🔄 synced file(s) with pimcore/sync-common-files (#178)
    * 🔄 synced file(s) with pimcore/sync-common-files
    
    * Update bundles.php
    
    * Update bundles.php
    
    ---------
    
    Co-authored-by: mcop1 <89011527+mcop1@users.noreply.github.com>

--- .github/ci/files/config/bundles.php
+++ .github/ci/files/config/bundles.php
@@ -1,6 +1,7 @@
 <?php
 
-return [
+return [    
     \Pimcore\Bundle\AdminBundle\PimcoreAdminBundle::class => ['all' => true],
-    \Pimcore\Bundle\PerspectiveEditorBundle\PimcorePerspectiveEditorBundle::class => ['all' => true]
+    \Pimcore\Bundle\PerspectiveEditorBundle\PimcorePerspectiveEditorBundle::class => ['all' => true],
+    \Pimcore\Bundle\SeoBundle\PimcoreSeoBundle::class => ['all' => true],
 ];
